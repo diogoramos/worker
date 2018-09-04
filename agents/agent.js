727028e2aacc1392edeb89ccfd75c70fb1ed0888
Diogo Ramos <diogo.ramos@sensedia.com>
2018-09-04 10:43:21 -0300

    [PATCH][ADD] - ADD one statement for upsert
    ADD one statement for upsert
    
    [AUTHOR] = Diogo Ramos <diogo.ramos@sensedia.com>
    [BRANCH] = dev

--- agents/agent.js
+++ agents/agent.js
@@ -1,8 +1,9 @@
-var config = require('../config/index.js');
-var connection = require('../repository/repository.js');
-var Twitter = require('twitter');
+const config = require('../config/index.js');
+const connection = require('../repository/repository.js');
+const Twitter = require('twitter');
+const Promise = require('promise');
 
-var client = new Twitter({
+const client = new Twitter({
     consumer_key: config.consumer_key,
     consumer_secret: config.consumer_secret,
     access_token_key: config.access_token_key,
@@ -15,27 +16,27 @@ process.on('message', function(hashtag) {
     },
     client.get('search/tweets', {q: hashtag, count:100}, function(error, tweets, response) {
         console.log('[agent.js] Agent com PID', process.pid , 'começou a busca e processamento da hashtag:', hashtag);
-        connection.connect();
+
         for(let i = 0; i < tweets.statuses.length; i++){
             let arr = tweets.statuses[i].user.location.split(",").map(function (val) {
                 return val;
               });
               
-              connection.query('INSERT INTO twitter_user SET ?', {
-                id: tweets.statuses[i].user.id,
-                name: '',//tweets.statuses[i].user.name,
-                screen_name: tweets.statuses[i].user.screen_name,
-                city: arr[0],
-                country: arr[1],
-                followers: tweets.statuses[i].user.followers_count,
-                lang: tweets.statuses[i].user.lang
-            }, 
-            function (error, results, fields) {
-                if (error) throw error;
-                console.log(results);
-              });
+              let userParams = [tweets.statuses[i].user.id, tweets.statuses[i].user.name, tweets.statuses[i].user.screen_name, arr[0], arr[1], tweets.statuses[i].user.followers_count, tweets.statuses[i].user.lang, '', tweets.statuses[i].user.screen_name, arr[0], arr[1], tweets.statuses[i].user.followers_count, tweets.statuses[i].user.lang];
+
+              connection.query('INSERT INTO twitter_user SET ID= ?, NAME= ?, SCREEN_NAME= ?, CITY= ?, COUNTRY= ?, FOLLOWERS= ?, LANG= ? ON DUPLICATE KEY UPDATE NAME= ?, SCREEN_NAME= ?, CITY= ?, COUNTRY= ?, FOLLOWERS= ?, LANG= ?',userParams, function(err,res){              
+                if(err) throw err;
+                console.log('Inserted user!');
+               });
+
+               let tweetParams = [tweets.statuses[i].id, tweets.statuses[i].create_at, tweets.statuses[i].text, tweets.statuses[i].lang, tweets.statuses[i].favorited, tweets.statuses[i].user.id , tweets.statuses[i].create_at, tweets.statuses[i].text, tweets.statuses[i].lang, tweets.statuses[i].favorited];
+
+               connection.query('INSERT INTO twitter_tweet SET ID= ?, CREATED_AT= ?, TEXT= ?, LANG= ?, FAVORITED= ?, TWITTER_USER_ID= ? ON DUPLICATE KEY UPDATE CREATED_AT= ?, TEXT= ?, LANG= ?, FAVORITED= ?',tweetParams, function(err,res){              
+                 if(err) throw err;
+                 console.log('Inserted tweet!');
+                });
+               
         }
-        connection.end();        
     }));
     process.disconnect();
 });
